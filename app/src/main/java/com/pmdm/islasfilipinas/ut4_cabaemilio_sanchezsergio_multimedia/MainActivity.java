28a171dc93d1fae01cd00ec16d25fdef44abb55b
ecabab <ecabab@gmail.com>
2017-02-15 16:28:41 +0100

    dfgh

--- app/src/main/java/com/pmdm/islasfilipinas/ut4_cabaemilio_sanchezsergio_multimedia/MainActivity.java
+++ app/src/main/java/com/pmdm/islasfilipinas/ut4_cabaemilio_sanchezsergio_multimedia/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MotionEvent;
 import android.widget.MediaController;
 import android.widget.VideoView;
+
+import java.io.File;
 
 
 public class MainActivity extends Activity {
@@ -18,23 +21,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Obtenemos la referencia al widget VideoView
         videoView = (VideoView) findViewById(R.id.videoView);
+
         // Creamos el objeto MediaController
         mediaController = new MediaController(this);
+
         // Establecemos el ancho del MediaController
         mediaController.setAnchorView(videoView);
+
         // Al contenedor VideoView le añadimos los controles
         videoView.setMediaController(mediaController);
         // Cargamos el contenido multimedia (el vídeo) en el VideoView
-        videoView.setVideoURI(Uri.parse("http://www.ted.com/talks/download/video/8584/talk/761"));
+        videoView.setVideoPath("http://www.ebookfrenzy.com/android_book/movie.mp4");
 
         // Registramos el callback que será invocado cuando el vídeo esté cargado y
         // preparado para la reproducción
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                mediaController.show(10000);
                 videoView.start();
             }
         });
