36f628e3a27c84137ca8c35646f95b77a75bbd68
SergoRz <sanchez13rz@gmail.com>
2017-02-15 15:44:12 +0100

    Initial commit

---
+++ app/src/main/java/com/pmdm/islasfilipinas/ut4_cabaemilio_sanchezsergio_multimedia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pmdm.islasfilipinas.ut4_cabaemilio_sanchezsergio_multimedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
